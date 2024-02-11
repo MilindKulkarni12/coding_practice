65d93aba6ffbc8de0769076bab1e550ef3946afc
Milind Kulkarni <milindrk12@gmail.com>
2024-02-10 21:07:00 -0500

    Time: 236 ms (93.56%) | Memory: 41.8 MB (29.72%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,21 +1,21 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-
         if total % 2 != 0:
-            # odd
             return False
+        dp = [[-1]*(total//2 + 1) for _ in range(len(nums)+1)]
 
-        # memoize
-        dp = [ [False]*(total//2 + 1) for _ in range(len(nums) + 1)]
-        for i in range(len(nums) + 1):
-            dp[i][0] = True
-        # print(dp)
-        for i in range(1, len(nums) + 1):
-            for t in range(1, total//2 + 1):
-                if nums[i - 1] <= t:
-                    dp[i][t] = dp[i - 1][t - nums[i - 1]] or dp[i - 1][t]
-                else:
-                    dp[i][t] = dp[i - 1][t]
-        return dp[len(nums)][total//2]
-        
+        def fx(i, rem):
+            if rem == 0:
+                return True
+            if i == 0:
+                return False
+            if dp[i][rem] != -1:
+                return dp[i][rem]
+            if nums[i] <= rem:
+                dp[i][rem] = fx(i-1, rem - nums[i]) or fx(i-1, rem)
+            else:
+                dp[i][rem] = fx(i-1, rem)
+            return dp[i][rem]
+
+        return fx(len(nums)-1, total//2) 
