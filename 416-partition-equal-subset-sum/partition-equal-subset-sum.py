c63902e120ecb10b47e911b9c8725cdeb10b8932
Milind Kulkarni <milindrk12@gmail.com>
2024-02-10 20:51:28 -0500

    Time: 269 ms (89.73%) | Memory: 41.9 MB (29.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,21 +1,33 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
+
         if total % 2 != 0:
+            # odd
             return False
-        dp = [[-1]*(total//2 + 1) for _ in range(len(nums)+1)]
 
-        def fx(i, rem):
-            if rem == 0:
+        # memoize
+        dp = [ [-1]*(total//2 + 1) for _ in range(len(nums) + 1)]
+
+        def fx(n, total):
+            if total == 0:
+                # reqd sum achieved
                 return True
-            if i == 0:
+            if n == 0:
+                # list parsed compeletely
                 return False
-            if dp[i][rem] != -1:
-                return dp[i][rem]
-            if nums[i] <= rem:
-                dp[i][rem] = fx(i-1, rem - nums[i]) or fx(i-1, rem)
-            else:
-                dp[i][rem] = fx(i-1, rem)
-            return dp[i][rem]
+            
+            # check if already parsed
+            if dp[n-1][total] != -1:
+                return dp[n-1][total]
 
-        return fx(len(nums)-1, total//2) 
+            if nums[n - 1] <= total:
+                # include OR exclude
+                dp[n-1][total] = fx(n - 1, total - nums[n - 1]) or fx(n - 1, total)
+            # move to next
+            else:
+                dp[n-1][total] = fx(n - 1, total)
+            
+            return dp[n-1][total]
+
+        return fx(len(nums), total //2)
