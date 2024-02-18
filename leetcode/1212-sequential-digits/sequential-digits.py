1e2b067f85b8e7f946c7bc2d7bd5861b110a7621
Milind Kulkarni <milindrk12@gmail.com>
2024-02-18 01:24:08 -0500

    Time: 32 ms (75.10%) | Memory: 16.5 MB (44.21%) - LeetSync

--- leetcode/1212-sequential-digits/sequential-digits.py
+++ leetcode/1212-sequential-digits/sequential-digits.py
@@ -1,25 +1,16 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
+        NUMS = "123456789"
         min_digits = len(str(low))
         max_digits = len(str(high))
         ans = []
-        for i in range(min_digits, max_digits+1):
-            # initialize starting digit
-            fd = 1
-            num = []
-            # initialize first num in the seq
-            for j in range(i):
-                num.append(fd) 
-                fd += 1
-            # fd = 5
-            curr_num = int(''.join([str(n) for n in num]))
-            if low <= curr_num <= high:
-                ans.append(curr_num) # 1234
-            while fd < 10:
-                num.append(fd) # 123456789
-                curr_num = int(''.join([str(n) for n in num[len(num)-i:]]))
-                if low <= curr_num <= high:
-                    ans.append(curr_num)
-                fd += 1
-            
+        curr_width = min_digits
+        while curr_width <= max_digits:
+            # print(curr_width)
+            for i in range(0, 10 - curr_width):
+                num = int(NUMS[i:i + curr_width])
+                if low <= num <= high:
+                    ans.append(num)
+            curr_width += 1
+        
         return ans
