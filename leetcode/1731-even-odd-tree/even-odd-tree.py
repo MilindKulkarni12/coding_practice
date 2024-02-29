6f9afb4b6466fc53275aa1a95a81b573861abfaa
Milind Kulkarni <milindrk12@gmail.com>
2024-02-29 18:32:06 -0500

    Time: 801 ms (5.03%) | Memory: 43.5 MB (19.72%) - LeetSync

--- leetcode/1731-even-odd-tree/even-odd-tree.py
+++ leetcode/1731-even-odd-tree/even-odd-tree.py
@@ -19,9 +19,7 @@ class Solution:
                 q.put(p.left)
             if p.right:
                 q.put(p.right)
-            
             p = q.get()
-            
             if p == root:
                 if not q.empty():
                     p = q.get()
@@ -37,19 +35,19 @@ class Solution:
             if i % 2 == 0:
                 # print(l)
                 prev = l[0]
-                if prev % 2 == 0:
+                if not (prev & 1):
                     return False
                 for j in range(1, len(l)):
                     # print(l[j])
-                    if l[j] % 2 == 0 or prev >= l[j]:
+                    if not (l[j] & 1) or prev >= l[j]:
                         return False
                     prev = l[j]
             else:
                 prev = l[0]
-                if prev % 2 != 0:
+                if prev & 1:
                     return False
                 for j in range(1, len(l)):
-                    if l[j] % 2 != 0 or prev <= l[j]:
+                    if l[j] & 1 or prev <= l[j]:
                         return False
                     prev = l[j]
         return True
