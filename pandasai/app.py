b96e7bf0d125abdb14319d559f7a0a66a22644dc
Milind Kulkarni <milindrk12@gmail.com>
2024-03-08 01:25:32 -0500

    minor changes

--- pandasai/app.py
+++ pandasai/app.py
@@ -5,13 +5,13 @@ from pandasai import SmartDataframe
 from pandasai.llm.openai import OpenAI
 
 API_KEY = os.environ.get('OPENAI_API_KEY')
-llm = OpenAI(api_token=API_KEY)
+llm = OpenAI(api_token=API_KEY, model_name='gpt-4-1106-preview')
 
 st.title("File Analyser with Pandas AI")
-uploaded_file = st.file_uploader("Upload a file for analysis", type=['csv'])
+uploaded_file = st.file_uploader("Upload a file for analysis", type=['xlsx', 'csv'])
 
 if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+    df = pd.read_excel(uploaded_file, header=None, names=['id', 'questions', 'answers', 'scores', 'justifications'])
     sdf = SmartDataframe(df, config= {'llm': llm})
     st.write(df.head())
 
@@ -19,7 +19,6 @@ if uploaded_file:
     if st.button("Submit"):
         if prompt:
             st.write("PandasAI is generating answer...")
-            st.write(sdf.chat(prompt))
+            st.write(sdf.chat(prompt, ))
         else:
             st.warning("Please enter a prompt.")
-
