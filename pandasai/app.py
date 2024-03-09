34cac865a542165f9d5e3e8c2eb1c5ff7c0a7cbc
Milind Kulkarni <milindrk12@gmail.com>
2024-03-09 00:03:27 -0500

    Added plotting of prompt output.

--- pandasai/app.py
+++ pandasai/app.py
@@ -3,22 +3,25 @@ import streamlit as st
 import pandas as pd
 from pandasai import SmartDataframe
 from pandasai.llm.openai import OpenAI
+import matplotlib
+
+matplotlib.use("TkAgg")
 
 API_KEY = os.environ.get('OPENAI_API_KEY')
 llm = OpenAI(api_token=API_KEY, model_name='gpt-4-1106-preview')
 
 st.title("File Analyser with Pandas AI")
-uploaded_file = st.file_uploader("Upload a file for analysis", type=['xlsx', 'csv'])
+uploaded_file = st.file_uploader("Upload a file for analysis", type=['csv'])
 
 if uploaded_file:
-    df = pd.read_excel(uploaded_file, header=None, names=['id', 'questions', 'answers', 'scores', 'justifications'])
+    df = pd.read_csv(uploaded_file)
     sdf = SmartDataframe(df, config= {'llm': llm})
     st.write(df.head())
 
     prompt = st.text_area("Enter Prompt")
     if st.button("Submit"):
         if prompt:
-            st.write("PandasAI is generating answer...")
-            st.write(sdf.chat(prompt, ))
+            with st.spinner("PandasAI is generating answer..."):
+                st.write(sdf.chat(prompt, ))
         else:
             st.warning("Please enter a prompt.")
