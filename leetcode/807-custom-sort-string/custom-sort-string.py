2bfd7417f85e575a308acaeb8bfda7304b3b0b51
Milind Kulkarni <milindrk12@gmail.com>
2024-03-11 01:24:29 -0400

    Time: 33 ms (73.85%) | Memory: 16.5 MB (91.57%) - LeetSync

--- leetcode/807-custom-sort-string/custom-sort-string.py
+++ leetcode/807-custom-sort-string/custom-sort-string.py
@@ -6,5 +6,5 @@ class Solution:
             if o_dict.get(c, -1) == -1:
                 o_dict[c] = i
                 i += 1 
-        ans = ''.join(sorted(s, key=lambda x: o_dict.get(x)))
+        ans = ''.join(sorted(s, key=lambda x: o_dict[x]))
         return ans
