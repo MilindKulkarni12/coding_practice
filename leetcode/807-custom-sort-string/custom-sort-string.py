d8f4a8f35577b970915283b2d5c907ed54701779
Milind Kulkarni <milindrk12@gmail.com>
2024-03-11 01:26:11 -0400

    Time: 29 ms (90.45%) | Memory: 16.4 MB (91.57%) - LeetSync

--- leetcode/807-custom-sort-string/custom-sort-string.py
+++ leetcode/807-custom-sort-string/custom-sort-string.py
@@ -2,9 +2,9 @@ class Solution:
     def customSortString(self, order: str, s: str) -> str:
         o_dict = {order[i]: i for i in range(len(order))}
         i = len(order)
-        for c in s:
-            if o_dict.get(c, -1) == -1:
-                o_dict[c] = i
-                i += 1 
-        ans = ''.join(sorted(s, key=lambda x: o_dict[x]))
+        # for c in s:
+        #     if o_dict.get(c, -1) == -1:
+        #         o_dict[c] = i
+        #         i += 1
+        ans = ''.join(sorted(s, key=lambda x: o_dict.get(x, -1)))
         return ans
