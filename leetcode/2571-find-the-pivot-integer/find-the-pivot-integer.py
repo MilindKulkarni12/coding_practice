de428944d4d5327b621126a9356da7e6a4d9f7d0
Milind Kulkarni <milindrk12@gmail.com>
2024-03-13 00:16:11 -0400

    Time: 61 ms (32.46%) | Memory: 16.6 MB (59.17%) - LeetSync

--- leetcode/2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ leetcode/2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,8 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
+        nsum = (n*(n+1))//2
         for x in range(n+1):
-            xsum = (x*(x+1))//2
-            nsum = (((2*x) + (n - x + 1 - 1))*(n - x + 1))//2 
-            if xsum == nsum:
+            xsum = (((2*x) + (n - x + 1 - 1))*(n - x + 1))//2 
+            if xsum == (nsum - xsum + x):
                 return x
         return -1
