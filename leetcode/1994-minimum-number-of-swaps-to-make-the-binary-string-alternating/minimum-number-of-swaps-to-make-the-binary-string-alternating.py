ef9bb3504d503c3cf09ad14dc6b917334b4b4e88
Milind Kulkarni <milindrk12@gmail.com>
2024-04-01 19:11:36 -0400

    Time: 36 ms (74.06%) | Memory: 16.8 MB (9.91%) - LeetSync

---
+++ leetcode/1994-minimum-number-of-swaps-to-make-the-binary-string-alternating/minimum-number-of-swaps-to-make-the-binary-string-alternating.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        """
+            1111000     
+            1010101        
+            
+            11110000 
+            10101010         
+            TFTFFTFT
+            
+            11110000
+            01010101
+            FTFTTFTF
+        """
+        n = len(s)
+        freq = Counter(s)
+        if n % 2 == 0:
+            if freq['0'] != freq['1']:
+                return -1
+        else:
+            if abs(freq['0'] - freq['1']) != 1:
+                return -1
+
+        if n % 2 != 0:
+            k = '1' if freq['1'] > freq['0'] else '0'
+            count = 0
+            for i, c in enumerate(s):
+                if c != k:
+                    count += 1
+                k = '0' if k == '1' else '1'
+        else:
+            k = '1'
+            count1 = count2 = 0
+            for i, c in enumerate(s):
+                if c != k:
+                    count1 += 1
+                k = '0' if k == '1' else '1'
+            k = '0'
+            for i, c in enumerate(s):
+                if c != k:
+                    count2 += 1
+                k = '0' if k == '1' else '1'
+            count = min(count1, count2)
+        return count // 2
