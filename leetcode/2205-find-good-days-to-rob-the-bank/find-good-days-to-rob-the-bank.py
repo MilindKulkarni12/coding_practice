b5b3ada13fc3b2b7aac96a730208eb0cbfae02e3
Milind Kulkarni <milindrk12@gmail.com>
2024-04-02 02:35:42 -0400

    Time: 686 ms (27.98%) | Memory: 36.2 MB (18.11%) - LeetSync

---
+++ leetcode/2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.py
@@ -0,0 +1,20 @@
+class Solution:
+    def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+        # dec = [0, 1, 2, 3, 0, 0, 1]
+        # inc = [0, 4, 3, 2, 1, 0, 0]
+        n = len(security)
+        inc, dec = [0]*n, [0]*n
+        i, j = 0, n - 1
+
+        while i < n:
+            inc[i] = inc[i-1] + 1 if i > 0 and security[i] <= security[i-1] else 0
+            dec[j] = dec[j+1] + 1 if j < n-1 and security[j] <= security[j+1] else 0
+            i += 1
+            j -= 1
+        # print(inc, dec)
+        ans = []
+        for i in range(time, n - time):
+            if inc[i] >= time and dec[i] >= time:
+                ans.append(i)
+        return ans
+
