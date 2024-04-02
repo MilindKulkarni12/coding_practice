f7a2cec025e19a16dc9f243dda4c9f345dd9834e
Milind Kulkarni <milindrk12@gmail.com>
2024-04-02 04:09:34 -0400

    Time: 363 ms (66.56%) | Memory: 30.2 MB (64.36%) - LeetSync

---
+++ leetcode/2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1 if nums[0] > nums[1] else 0
+        
+        mn = nums.index(min(nums))
+        mx = max(nums)
+        for i in range(n-1, -1, -1):
+            if nums[i] == mx:
+                mx = i
+                break
+
+        print(mn, mx)
+        swaps = mn + n - mx - 1
+        if mn > mx:
+            swaps -= 1
+        return swaps
