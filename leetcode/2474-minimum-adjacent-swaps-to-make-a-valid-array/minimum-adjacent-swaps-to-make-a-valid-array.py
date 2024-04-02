eb33552145a794043570e1f9f59e16526a18e507
Milind Kulkarni <milindrk12@gmail.com>
2024-04-02 04:13:42 -0400

    Time: 380 ms (20.41%) | Memory: 30.2 MB (64.36%) - LeetSync

--- leetcode/2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ leetcode/2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -6,14 +6,18 @@ class Solution:
         elif n == 2:
             return 1 if nums[0] > nums[1] else 0
         
+        mx = n - nums[::-1].index(max(nums)) - 1
+        # print(mx, end=' ')
+        
+        
         mn = nums.index(min(nums))
-        mx = max(nums)
-        for i in range(n-1, -1, -1):
-            if nums[i] == mx:
-                mx = i
-                break
+        # mx = max(nums)
+        # for i in range(n-1, -1, -1):
+        #     if nums[i] == mx:
+        #         mx = i
+        #         break
 
-        print(mn, mx)
+        # print(mx)
         swaps = mn + n - mx - 1
         if mn > mx:
             swaps -= 1
