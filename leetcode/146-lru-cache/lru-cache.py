2a8827754b5b87da2c09bad3233d62e1e8033815
Milind Kulkarni <milindrk12@gmail.com>
2024-04-03 18:40:07 -0400

    Time: 3103 ms (5.01%) | Memory: 77.8 MB (45.73%) - LeetSync

---
+++ leetcode/146-lru-cache/lru-cache.py
@@ -0,0 +1,38 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = 0
+        self.max_cap = capacity
+        self.lru = {}
+        self.time = 0
+
+    def get(self, key: int) -> int:
+        self.time += 1
+        if self.lru.get(key, -1) != -1:
+            self.lru[key][1] = self.time
+            return self.lru[key][0]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        self.time += 1
+        if self.lru.get(key, False): # existing
+            self.lru[key] = [value, self.time]
+        else: # new
+            if self.cap == self.max_cap:
+                # remove lru
+                oldest = 1000000
+                idx = -1
+                for k, v in self.lru.items():
+                    if v[1] < oldest:
+                        oldest = v[1]
+                        idx = k
+                self.lru.pop(idx)
+                self.lru[key] = [value, self.time]
+            else:
+                self.lru[key] = [value, self.time]
+                self.cap += 1
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
