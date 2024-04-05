d6551dd84b9caa4f237ddbe4213b0f72162e43ce
Milind Kulkarni <milindrk12@gmail.com>
2024-04-05 03:01:53 -0400

    Time: 35 ms (85.33%) | Memory: 16.6 MB (64.04%) - LeetSync

--- leetcode/139-word-break/word-break.py
+++ leetcode/139-word-break/word-break.py
@@ -1,5 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        # def fx(curr_str, i):
+        #     if len(curr_str) == curr_str.count('*'):    
+        #         return True
+        #     if i >= len(wordDict):
+        #         return False
+        #     if wordDict[i] in curr_str:
+        #         curr_str1 = curr_str.replace(wordDict[i], '*', 1)
+        #         return fx(curr_str1, i) or fx(curr_str, i+1)
+        #     else:
+        #         return fx(curr_str, i+1)
+        # return fx(s, 0)
         n = len(s)
         words = set(wordDict)
         dp = [False]*(n+1)
@@ -11,3 +23,4 @@ class Solution:
                     dp[i] = True
                     break
         return dp[-1]
+
