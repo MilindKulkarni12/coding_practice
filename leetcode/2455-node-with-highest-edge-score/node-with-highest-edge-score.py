4c4745635118119010db8f532dcbf48324e942c0
Milind Kulkarni <milindrk12@gmail.com>
2024-04-07 03:15:56 -0400

    Time: 949 ms (33.45%) | Memory: 34.1 MB (27.76%) - LeetSync

--- leetcode/2455-node-with-highest-edge-score/node-with-highest-edge-score.py
+++ leetcode/2455-node-with-highest-edge-score/node-with-highest-edge-score.py
@@ -1,7 +1,14 @@
 class Solution:
     def edgeScore(self, edges: List[int]) -> int:
         scores = defaultdict(int)
+        max_score_idx, max_score = -1, -1
         for i in range(len(edges)):
             scores[edges[i]] += i
-        # print(scores)
-        return list(sorted(scores.items(), key=lambda x: (x[1], -x[0]), reverse=True))[0][0]
+            if scores[edges[i]] > max_score:
+                max_score = scores[edges[i]]
+                max_score_idx = edges[i]
+            elif scores[edges[i]] == max_score:
+                max_score_idx = min(max_score_idx, edges[i])
+        # print(scores, max_score, max_score_idx)
+        # return list(sorted(scores.items(), key=lambda x: (x[1], -x[0]), reverse=True))[0][0]
+        return max_score_idx
