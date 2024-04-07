11b7e945d618377663a6a25a097dbd74e5cd0834
Milind Kulkarni <milindrk12@gmail.com>
2024-04-07 03:24:58 -0400

    Time: 44 ms (19.86%) | Memory: 16.8 MB (16.95%) - LeetSync

--- leetcode/2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.py
+++ leetcode/2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.py
@@ -1,12 +1,18 @@
 class Solution:
     def numberOfPairs(self, nums: List[int]) -> List[int]:
-        pair = defaultdict(bool)
-        pair_count = 0
+        # pair = defaultdict(bool)
+        # pair_count = 0
         n = len(nums)
-        for num in nums:
-            if pair.get(num, False):
-                pair[num] = False
-                pair_count += 1
-            else:
-                pair[num] = True
-        return [pair_count, n - 2*pair_count]
+        # for num in nums:
+        #     if pair.get(num, False):
+        #         pair[num] = False
+        #         pair_count += 1
+        #     else:
+        #         pair[num] = True
+        # return [pair_count, n - 2*pair_count]
+        freq = Counter(nums)
+        p = 0
+        for v in freq.values():
+            p += v // 2
+        return [p, n-2*p]
+
