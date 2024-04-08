14fa1a4c7cabcc2c4bf9787b6397f67e20a40be3
Milind Kulkarni <milindrk12@gmail.com>
2024-04-08 04:30:41 -0400

    Time: 38 ms (72.82%) | Memory: 16.7 MB (16.84%) - LeetSync

--- leetcode/1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ leetcode/1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -7,14 +7,15 @@ class Solution:
         print(n)
         while j < n:
             # print('i:', i, 'students[i]:', students[i], 'j:', j, 'sandwiches[j]:', sandwiches[j], last_found_i)
+            
             if students[i] == sandwiches[j] and students[i] != -1:
                 students[i] = -1
                 last_found_i = i
                 i = (i + 1) % n
                 j += 1
+            elif i == n-1 and last_found_i == -1:
+                return n
             else:
-                if i == n-1 and last_found_i == -1:
-                    return n
                 if i == last_found_i:
                     return n - j
                 i = (i + 1) % n
