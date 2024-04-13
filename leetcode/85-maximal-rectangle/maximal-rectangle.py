ba18f04c35df78318fabe7d247577f62e8c881c5
Milind Kulkarni <milindrk12@gmail.com>
2024-04-13 16:10:49 -0400

    Time: 211 ms (84.20%) | Memory: 18 MB (34.51%) - LeetSync

--- leetcode/85-maximal-rectangle/maximal-rectangle.py
+++ leetcode/85-maximal-rectangle/maximal-rectangle.py
@@ -1,17 +1,34 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        maxarea = 0
+        def get_max_area(histogram):
+            n = len(histogram)
+            stack = []
+            mx_area = 0
+            for i, h in enumerate(histogram):
+                start = i
+                while stack and stack[-1][1] > h:
+                    idx, top_h = stack.pop()
+                    mx_area = max(mx_area, top_h * (i - idx))
+                    start = idx
+                stack.append((start, h))
+            
+            for i, h in stack:
+                mx_area = max(mx_area, h * (n - i))
+            
+            return mx_area
 
-        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == '0': continue
 
-                # compute the maximum width and update dp with it
-                width = dp[i][j] = dp[i][j-1] + 1 if j else 1
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-                # compute the maximum area rectangle with a lower right corner at [i, j]
-                for k in range(i, -1, -1):
-                    width = min(width, dp[k][j])
-                    maxarea = max(maxarea, width * (i-k+1))
-        return maxarea
+        histogram = [0]*cols
+        max_area = 0
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    histogram[j] += 1
+                else:
+                    histogram[j] = 0
+
+            max_area = max(max_area, get_max_area(histogram))
+        return max_area
