a1963a0182978551cd5906cdba8a6cb991159ba3
Milind Kulkarni <milindrk12@gmail.com>
2024-04-19 16:11:54 -0400

    Time: 354 ms (8.89%) | Memory: 30.2 MB (80.73%) - LeetSync

--- leetcode/41-first-missing-positive/first-missing-positive.py
+++ leetcode/41-first-missing-positive/first-missing-positive.py
@@ -1,16 +1,13 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        found = [False]*len(nums)
         n = len(nums)
-        # First pass: filter and restructure the array
-        for i in range(n):
-            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-                # Swap the elements to their correct positions
-                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
 
-        # Second pass: find the first missing positive integer
-        for i in range(n):
-            if nums[i] != i + 1:
-                return i + 1
-
-        # If no missing positive integer in the range [1, n]
-        return n + 1
+        for num in nums:
+            if 0 < num <= n:
+                found[num-1] = True
+        # print(found)
+        for i, f in enumerate(found):
+            if not f:
+                return i+1
+        return n+1
