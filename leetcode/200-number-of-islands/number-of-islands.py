5ed6dcec88da818b1993d4f96d670fd37fde47be
Milind Kulkarni <milindrk12@gmail.com>
2024-04-18 21:16:23 -0400

    Time: 226 ms (89.81%) | Memory: 18.9 MB (61.54%) - LeetSync

--- leetcode/200-number-of-islands/number-of-islands.py
+++ leetcode/200-number-of-islands/number-of-islands.py
@@ -1,14 +1,13 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        grid_copy = grid[::]
         n = len(grid)
         m = len(grid[0])
         count = 0
 
         def floodfill(i, j):
-            if i >= 0 and i < n and j >= 0 and j < m and grid_copy[i][j] == '1':
+            if i >= 0 and i < n and j >= 0 and j < m and grid[i][j] == '1':
                 # print(i, j)
-                grid_copy[i][j] = '0'
+                grid[i][j] = '0'
                 floodfill(i-1, j)
                 floodfill(i+1, j)
                 floodfill(i, j-1)
@@ -16,7 +15,7 @@ class Solution:
             
         for i in range(n):
             for j in range(m):
-                if grid_copy[i][j] == '1':
+                if grid[i][j] == '1':
                     count += 1
                     floodfill(i, j)
         
