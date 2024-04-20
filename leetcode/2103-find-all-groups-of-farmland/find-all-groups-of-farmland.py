da178cc8398d840578a58ecc453619f5c2ab3bad
Milind Kulkarni <milindrk12@gmail.com>
2024-04-20 00:38:29 -0400

    Time: 936 ms (39.00%) | Memory: 41.6 MB (17.50%) - LeetSync

--- leetcode/2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ leetcode/2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -2,7 +2,6 @@ class Solution:
     def findFarmland(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         m = len(grid[0])
-        count = 0
 
         def floodfill(i, j):
             if i >= 0 and i < n and j >= 0 and j < m and grid[i][j] == 1:
@@ -21,9 +20,7 @@ class Solution:
                 if grid[i][j] == 1:
                     min_x, min_y = i, j
                     max_x, max_y = floodfill(i, j)
-                    max_x = max(min_x, max_x)
-                    max_y = max(min_y, max_y)
-
+                    
                     res.append([min_x, min_y, max_x, max_y])
         
         return res
