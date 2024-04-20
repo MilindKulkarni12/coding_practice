dc3f69e8fa8695dfa6823767aa68df7b5727dc1b
Milind Kulkarni <milindrk12@gmail.com>
2024-04-20 02:27:01 -0400

    Time: 45 ms (24.16%) | Memory: 16.9 MB (34.46%) - LeetSync

---
+++ leetcode/133-clone-graph/clone-graph.py
@@ -0,0 +1,44 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        """
+            {
+                1: node1;
+                2: node2;
+                3: node3;
+                4: node4;
+            }
+
+            1 -> 2, 4
+            2 -> 3, 1
+            3 -> 2, 4
+            4 -> 3, 1
+        """
+        if node is None:
+            return None
+
+        node_dict = {node.val: Node(node.val)}
+        start = node
+        q = deque([start])
+        while q:
+            n = q.popleft()
+            new_node = node_dict[n.val]
+
+            for nei in n.neighbors:
+                if nei.val in node_dict:
+                    new_node.neighbors.append(node_dict[nei.val])
+                else:
+                    new_nei_node = Node(nei.val)
+                    node_dict[nei.val] = new_nei_node
+                    new_node.neighbors.append(new_nei_node)
+                    q.append(nei)
+        # print(node_dict)
+        return node_dict[node.val]
