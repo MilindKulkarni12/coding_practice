07ed0a3192b53b00cfa75ce0b84be7c671522e27
Milind Kulkarni <milindrk12@gmail.com>
2024-04-20 00:55:56 -0400

    Time: 56 ms (6.13%) | Memory: 19.4 MB (66.92%) - LeetSync

--- leetcode/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ leetcode/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,13 +6,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.inorder_list = []
-
-        def inorder(root):
+        # self.inorder_list = []
+        self.ans = 0
+        self.k = k
+        def inorder(root, k):
             if root:
-                inorder(root.left)
-                self.inorder_list.append(root.val)
-                inorder(root.right)
+                inorder(root.left, self.k)
+                self.k -= 1
+                if self.k == 0:
+                    self.ans = root.val
+                    return
+                # self.inorder_list.append(root.val)
+                inorder(root.right, self.k)
         # print(self.inorder_list)
-        inorder(root)
-        return self.inorder_list[k-1]
+        inorder(root, self.k)
+        return self.ans # self.inorder_list[k-1]
