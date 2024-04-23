0bf49fae6ca0329d3aa269c1e97f12e2bdfeb1b0
Milind Kulkarni <milindrk12@gmail.com>
2024-04-23 18:18:06 -0400

    Time: 416 ms (52.25%) | Memory: 26.5 MB (91.84%) - LeetSync

--- leetcode/310-minimum-height-trees/minimum-height-trees.py
+++ leetcode/310-minimum-height-trees/minimum-height-trees.py
@@ -1,38 +1,26 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        # edge cases
-        if n <= 2:
-            return [i for i in range(n)]
+        adj_list = defaultdict(list)
+        for v1, v2 in edges:
+            adj_list[v1].append(v2)
+            adj_list[v2].append(v1)
 
-        # Build the graph with the adjacency list
-        neighbors = [set() for i in range(n)]
-        for start, end in edges:
-            neighbors[start].add(end)
-            neighbors[end].add(start)
-
-        # Initialize the first layer of leaves
-        leaves = []
-        for i in range(n):
-            if len(neighbors[i]) == 1:
-                leaves.append(i)
-
-        # Trim the leaves until reaching the centroids
-        remaining_nodes = n
-        while remaining_nodes > 2:
-            remaining_nodes -= len(leaves)
-            new_leaves = []
-            # remove the current leaves along with the edges
-            while leaves:
-                leaf = leaves.pop()
-                # the only neighbor left for the leaf node
-                neighbor = neighbors[leaf].pop()
-                # remove the only edge left
-                neighbors[neighbor].remove(leaf)
-                if len(neighbors[neighbor]) == 1:
-                    new_leaves.append(neighbor)
-
-            # prepare for the next round
-            leaves = new_leaves
-
-        # The remaining nodes are the centroids of the graph
-        return leaves
+        leaves = deque()
+        node_count = {}
+        for v, nei in adj_list.items():
+            edge_count = len(nei)
+            if edge_count == 1:
+                leaves.append(v)
+            node_count[v] = edge_count
+        
+        while leaves:
+            if n <= 2:
+                return list(leaves)
+            for _ in range(len(leaves)):
+                leaf = leaves.popleft()
+                n -= 1
+                for nei in adj_list[leaf]:
+                    node_count[nei] -= 1
+                    if node_count[nei] == 1:
+                        leaves.append(nei)
+        return [0]
