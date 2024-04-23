5d060a41710895b7154d20e08e64bedd11cf642a
Milind Kulkarni <milindrk12@gmail.com>
2024-04-23 18:23:00 -0400

    Time: 421 ms (45.02%) | Memory: 26.3 MB (95.19%) - LeetSync

--- leetcode/310-minimum-height-trees/minimum-height-trees.py
+++ leetcode/310-minimum-height-trees/minimum-height-trees.py
@@ -12,7 +12,14 @@ class Solution:
             if edge_count == 1:
                 leaves.append(v)
             node_count[v] = edge_count
-        
+        """
+            l = 0, 1, 2, 5; n = 6
+            l = 1, 2, 5; n = 5
+            l = 2, 5; n = 4
+            l = 2, 5, 3; n = 4
+            l = 5, 3; n = 3
+            l = 3, 4; n = 2
+        """
         while leaves:
             if n <= 2:
                 return list(leaves)
