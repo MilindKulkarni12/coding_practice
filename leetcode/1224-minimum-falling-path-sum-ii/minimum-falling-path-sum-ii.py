7be16f1ea1adde43cac2b23981ce116ad83bbd0b
Milind Kulkarni <milindrk12@gmail.com>
2024-04-26 02:39:36 -0400

    Time: 625 ms (47.34%) | Memory: 19.9 MB (80.97%) - LeetSync

--- leetcode/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
+++ leetcode/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
@@ -11,14 +11,14 @@ class Solution:
         #     prev_row = curr_row
         # return min(prev_row)
         
-        
         size = len(grid)
         prev_row = [0] * size
         for row in grid:
             curr_row = [0] * size
             for i, n in enumerate(row):
                 curr_row[i] = n
-                if prev_row[0:i] + prev_row[i+1:size]:
-                    curr_row[i] += min(prev_row[0:i] + prev_row[i+1:size])
+                lst = prev_row[0:i] + prev_row[i+1:size]
+                if lst:
+                    curr_row[i] += min(lst)
             prev_row = curr_row
         return min(prev_row)
