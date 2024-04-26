6457f235f4551f4de06790645119555421be4b8a
Milind Kulkarni <milindrk12@gmail.com>
2024-04-26 02:34:25 -0400

    Time: 774 ms (36.72%) | Memory: 20.2 MB (34.96%) - LeetSync

---
+++ leetcode/1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        # size = len(matrix)
+        # prev_row = [0] * size
+        # for row in matrix:
+        #     curr_row = [0] * size
+        #     for i, n in enumerate(row):
+        #         l = max(0, i-1)
+        #         r = min(size, i+2)
+        #         curr_row[i] = min(prev_row[l:r]) + n
+        #     prev_row = curr_row
+        # return min(prev_row)
+        
+        
+        size = len(grid)
+        prev_row = [0] * size
+        for row in grid:
+            curr_row = [0] * size
+            for i, n in enumerate(row):
+                curr_row[i] = n
+                if prev_row[0:i] + prev_row[i+1:size]:
+                    curr_row[i] += min(prev_row[0:i] + prev_row[i+1:size])
+            prev_row = curr_row
+        return min(prev_row)
