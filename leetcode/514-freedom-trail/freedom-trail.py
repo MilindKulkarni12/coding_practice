348d24760528a57f15ec1a5128a3fc51387a6b40
Milind Kulkarni <milindrk12@gmail.com>
2024-04-27 03:16:09 -0400

    Time: 213 ms (40.78%) | Memory: 17.4 MB (43.14%) - LeetSync

---
+++ leetcode/514-freedom-trail/freedom-trail.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        dp = {}
+
+        def fx(r, k):
+            if k == len(key):
+                return 0
+            
+            if (r, k) in dp:
+                return dp[(r, k)]
+
+            res = float("inf")
+            for i, c in enumerate(ring):
+                if c == key[k]: # found
+                    min_dist = min(
+                        abs(r - i),
+                        len(ring) - abs(r - i)
+                    )
+
+                    res = min(
+                        res, 
+                        min_dist + 1 + fx(i, k + 1)
+                    )
+            dp[(r, k)] = res
+            return res
+        return fx(0, 0)
+
