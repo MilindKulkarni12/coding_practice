10201db7ff9c74cbf649df6b7bcf4b495077369f
Milind Kulkarni <milindrk12@gmail.com>
2024-04-27 02:11:17 -0400

    Time: 718 ms (77.58%) | Memory: 26.1 MB (32.38%) - LeetSync

--- leetcode/1830-count-good-meals/count-good-meals.py
+++ leetcode/1830-count-good-meals/count-good-meals.py
@@ -1,13 +1,5 @@
 class Solution:
     def countPairs(self, deli: List[int]) -> int:
-        # powers = set([2**i for i in range(0, 22)])
-        # freq = defaultdict(int)
-        # ans = 0
-        # for d in deli:
-        #     for p in powers:
-        #         ans += freq[p - d]
-        #     freq[d] += 1
-        # return ans 
         powers = set([2**i for i in range(0, 22)])
         freq = Counter(deli)
         ans = 0
@@ -15,7 +7,7 @@ class Solution:
         for d in freq.keys():
             for p in powers:
                 if (p - d) in freq and (d, p - d) not in done:
-                    done[(d, p-d)] = True
+                    # done[(d, p-d)] = True
                     done[(p-d, d)] = True
                     
                     count1 = freq[p-d]
