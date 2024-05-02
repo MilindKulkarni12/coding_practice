7e5a47ba40bd809d079921d1b292c451a581c633
Milind Kulkarni <milindrk12@gmail.com>
2024-05-01 21:11:32 -0400

    Time: 37 ms (45.09%) | Memory: 16.5 MB (85.54%) - LeetSync

---
+++ leetcode/119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        pascal_matrix = [1, 1]
+        if rowIndex < 2:
+            return pascal_matrix[:rowIndex+1]
+        
+        for i in range(2,rowIndex+1):
+            row=[1] + [pascal_matrix[j] + pascal_matrix[j+1] for j in range(i-1)] + [1]
+            pascal_matrix = row
+        
+        return pascal_matrix
+        
