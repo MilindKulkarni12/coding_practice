385d8b63654f5006d29bac3494dada0f202493db
Milind Kulkarni <milindrk12@gmail.com>
2024-05-04 02:54:16 -0400

    Time: 1003 ms (88.08%) | Memory: 62.9 MB (26.81%) - LeetSync

--- leetcode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ leetcode/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -25,12 +25,11 @@ class Solution:
         #sorted_points=sorted(points, key=lambda x:x[1])
         points.sort(key=lambda x:x[1])
         #print(points)
-        last=float('-inf')
-        ans=0
+        last = float('-inf')
+        ans = 0
         for point in points:
-            start=point[0]
-            end=point[1]
-            if start>last:
-                ans=ans+1
-                last=end
+            start, end = point
+            if start > last:
+                ans += 1
+                last = end
         return ans
