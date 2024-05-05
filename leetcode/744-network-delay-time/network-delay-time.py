45566ec65ce9074bc80650730c496a007332fe3e
Milind Kulkarni <milindrk12@gmail.com>
2024-05-05 04:08:32 -0400

    Time: 363 ms (79.24%) | Memory: 18.5 MB (66.05%) - LeetSync

--- leetcode/744-network-delay-time/network-delay-time.py
+++ leetcode/744-network-delay-time/network-delay-time.py
@@ -1,36 +1,27 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        paths = []
         adj_list = defaultdict(list)
-        heapq.heappush(paths, (0, k, k))
         for s, e, w in times:
             adj_list[s].append((e, w))
 
         # print(adj_list)
+        paths = []
         vis = set()
         total_time = 0
-        time_dict = defaultdict(int)
+        heapq.heappush(paths, (0, k))
         
         while paths and len(vis) < n:
             # print(paths)
-            curr_time, src, end = heapq.heappop(paths)
+            curr_time, src = heapq.heappop(paths)
             total_time = curr_time
-            if end in vis:
+            if src in vis:
                 continue
 
-            vis.add(end)
-            time_dict[src] = max(time_dict[src], curr_time)
+            vis.add(src)
             
             # print(time_dict)
-            for nxt, wt in adj_list[end]:
+            for nxt, wt in adj_list[src]:
                 if nxt not in vis:
-                    heapq.heappush(paths, (total_time + wt, end, nxt))
+                    heapq.heappush(paths, (total_time + wt, nxt))
         
         return total_time if len(vis) == n else -1
-
-        """
-            1 -- 2
-            |  /
-            | /
-            3
-        """
