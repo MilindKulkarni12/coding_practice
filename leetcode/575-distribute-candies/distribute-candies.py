2fb4d34df95ee1d4359734adfc39cc1cd9a55987
Milind Kulkarni <milindrk12@gmail.com>
2024-05-06 02:27:13 -0400

    Time: 609 ms (46.47%) | Memory: 18.4 MB (92.26%) - LeetSync

--- leetcode/575-distribute-candies/distribute-candies.py
+++ leetcode/575-distribute-candies/distribute-candies.py
@@ -1,14 +1,5 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         freq = Counter(candyType)
-        n = len(candyType)
-        
-        unique = 0
-        for k, v in freq.items():
-            if v < 2:
-                unique += 1
-        
-        if len(freq) >= (n // 2):
-            return n // 2
-        
-        return len(freq) 
+        n = len(candyType) // 2
+        return n if len(freq) >= n else len(freq)
